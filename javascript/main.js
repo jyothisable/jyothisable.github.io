59adee9a8c3217046479c2d7e4d6bafb6663c926
athul jyothis <63841734+athulJ18@users.noreply.github.com>
2021-06-17 19:12:32 +0530

    responsive mobile nav ready

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,25 @@
-const menu = document.querySelector('.mobileNav')
+const menu = document.querySelector(".menu");
 const ham = document.querySelector('.ham')
-const close = document.querySelector('.fa-close')
-const open = document.querySelector('.fa-bars')
+const closeIcon = document.querySelector('.fa-close')
+const openIcon = document.querySelector('.fa-bars')
 
-ham.onclick = toggle()
+ham.onclick = toggleMenu
 
-function toggle() {
+function toggleMenu(){
     if (menu.classList.contains("showNav")) {
     menu.classList.remove("showNav");
-    close.style.display = "none";
-    open.style.display = "block";
+    closeIcon.style.display = "none";
+    openIcon.style.display = "block";
     } else {
-    menu.classList.add("showMenu");
-    close.style.display = "block";
-    open.style.display = "none";
+    menu.classList.add("showNav");
+    closeIcon.style.display = "block";
+    openIcon.style.display = "none";
     }
 }
+
+const menuLinks = document.querySelectorAll(".menu>li");
+
+menuLinks.forEach(
+  (menuLink) => (menuLink.onclick = toggleMenu)
+);
+
